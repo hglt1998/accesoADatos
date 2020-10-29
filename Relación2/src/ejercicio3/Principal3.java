2785d9a9c82f3d5c2e3c80952f8b1819a29c6853
hglt1998 <hglt1998@humbertpc>
2020-10-29 18:21:11 +0100

    ejercicio3

--- Relación2/src/ejercicio3/Principal3.java
+++ Relación2/src/ejercicio3/Principal3.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 
+//import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
 
 
-public class Principal3 {
+public class Principal3 extends DefaultHandler{
 
 	private static final String NOMBRE_FICHERO = "resultados.xml";
 	
@@ -25,7 +27,7 @@ public class Principal3 {
 		
 		XMLReader reader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
 		
-		reader.setContentHandler(new GestionContenido3());
+//		reader.setContentHandler(new ContentHandler());
 		//Arranca el parser leyendo el fichero XML
 		reader.parse(new InputSource(nombreFichero));
 	}
